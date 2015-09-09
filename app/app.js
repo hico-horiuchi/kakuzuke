5ca73f457626727bf394f2e23442df0c39090819
Akihiko Horiuchi <12ff5b8@gmail.com>
2015-09-09 12:28:56 +0900

    :1234: show only status code when error

--- app/app.js
+++ app/app.js
@@ -2,8 +2,7 @@ var top = top || {};
 var ranking = ranking || {};
 
 var extractStatus = function(xhr, xhrOptions) {
-  var isJson = "\"[{".indexOf(xhr.responseText.charAt(0)) !== -1;
-  return isJson ? xhr.responseText : JSON.stringify(xhr.responseText);
+  return xhr.status !== 200 ? String(xhr.status) : xhr.responseText;
 };
 
 (function(window) {
